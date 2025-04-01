e9d5370c3d18b81cc2b306c6f6fee89fab10e3de
ahmedbenabdelaziz <ahmedaba1652001@gmail.com>
2025-04-01 19:23:04 +0100

    Ajout du dossier nom_du_dossier

--- routes/employee.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { createEmployee, getEmployees } from '../controllers/employeeController.js';
-
-const router = express.Router();
-
-router.get('/', getEmployees);
-router.post('/', createEmployee);
-
-export default router;
